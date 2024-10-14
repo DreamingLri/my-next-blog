3172d61962472c6c5dc52a7a4fb0bdbc0e8b20bf
DreamingLri <DreamingLri@users.noreply.github.com>
2024-10-14 09:50:42 +0000

    deploy: d69eb0b65328287de90fb14bcdcba4e1509c1f70

---
+++ assets/YunGallery-CGyFT9Ab.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-B7QMtqiY.js";import"./app-0JkXls-E.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-yDyZ4WjG.js";export{o as default};
---
+++ assets/back-evxGO8W7.js
@@ -0,0 +1,1 @@
+import{u as o}from"./app-0JkXls-E.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
